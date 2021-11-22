463ed4c6dc2db7ecf82bae28b7a5792dc33791e1
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-22 11:37:53 +0600

    Add files via upload

---
+++ check positive or negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter an number :");
+    scanf("%d",&num);
+
+    if(num>0)
+        printf("Positive number");
+    else if(num<0)
+        printf("Negative  number");
+    else
+        printf("Number is Zero");
+
+        getch();
+}
